32a2d8e9119fa6d96fe8e1cf7e5597e16a215b9f
Sergey Karbainov <karbainovs@mail.ru>
2016-08-21 23:37:59 +0800

    try fix symlimk wifi

--- Android.mk
+++ Android.mk
@@ -151,10 +151,10 @@ $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9306; \
     ln -sf /data/misc/audio/mbhc.bin \
     $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_mbhc.bin)
 
-# Create a link for the WCNSS config file, which ends up as a writable
-# version in /data/misc/wifi
-$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
+$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_wlan_nv.bin \
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
-    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
+    $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
 endif
