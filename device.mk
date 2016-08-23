39a18e9040df9c842643d2ce0c58503896f36a0d
Sergey Karbainov <SPORTAK123@gmail.com>
2016-08-23 15:19:05 +0800

    Start audio fixing

--- device.mk
+++ device.mk
@@ -48,7 +48,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml
 
 # Camera
 PRODUCT_PACKAGES += \
