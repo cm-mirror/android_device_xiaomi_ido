bee22de783e33fd37fa7c99772e6974805780cbe
Sergey Karbainov <karbainovs@mail.ru>
2016-08-24 10:45:50 +0800

    Fix inrared port

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
@@ -56,6 +57,10 @@ PRODUCT_PACKAGES += \
     camera.msm8916 \
     libmm-qcamera \
     Snap
+
+# Consumerir
+PRODUCT_PACKAGES += \
+    consumerir.default
 
 # Display
 PRODUCT_COPY_FILES += \
