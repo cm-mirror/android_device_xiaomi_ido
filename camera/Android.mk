a1ac80ceba66c22558fc860a13777bc2c4a6f7a0
Sergey Karbainov <SPORTAK123@gmail.com>
2016-08-26 00:37:13 +0800

    Try fix build

--- camera/Android.mk
+++ camera/Android.mk
@@ -9,10 +9,11 @@ LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libgui libutils
+    libhardware liblog libcamera_client libgui libutils
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
